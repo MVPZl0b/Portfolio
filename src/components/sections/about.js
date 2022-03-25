afa5bfa89413c0663121bcc3eca2e4e13e18f309
mvpzl0b <zack.bellerose@gmail.com>
2022-03-25 19:12:15 -0400

    Added animations and formatted about me

--- src/components/sections/about.js
+++ src/components/sections/about.js
@@ -1,14 +1,15 @@
-import React from 'react'
-import styled from "styled-components"
-import { StaticImage } from 'gatsby-plugin-image'
-import ContentWrapper from '../../styles/contentWrapper'
+import React from "react";
+import styled from "styled-components";
+import ContentWrapper from "../../styles/contentWrapper";
+import "animate.css/animate.min.css";
+import { AnimationOnScroll } from "react-animation-on-scroll";
 
 const StyledSection = styled.section`
   width: 100%;
   height: auto;
   background: #263238;
   margin-top: 4rem;
-`
+`;
 const StyledContentWrapper = styled(ContentWrapper)`
   && {
     width: 100%;
@@ -19,16 +20,16 @@ const StyledContentWrapper = styled(ContentWrapper)`
     @media (min-width: 786px) {
       flex-direction: row;
       justify-content: space-between;
-    } 
+    }
     .section-title {
-      margin-bottom: 0.5rem;
-      text-align: center;
-      color: #607D8B;
+      margin-bottom: 1.25rem;
+      text-align: left;
+      color: #607d8b;
     }
     .section-subtitle {
       margin-top: 0.5rem;
-      text-align: center;
-      color: #607D8B;
+      text-align: left;
+      color: #607d8b;
     }
     .inner-wrapper {
       display: flex;
@@ -38,27 +39,31 @@ const StyledContentWrapper = styled(ContentWrapper)`
     .text-content {
       width: 100%;
       max-width: 43.25rem;
-      color: #607D8B;
+      color: #607d8b;
       box-shadow: 0 1px 6px rgba(0, 0, 0, 0.12), 0 1px 4px rgba(0, 0, 0, 0.24);
       transition: all 0.3s ease-out;
-        &:hover {
-          transform: translate3d(0px, -0.155rem, 0px);
-        }
+      &:hover {
+        transform: translate3d(0px, -0.155rem, 0px);
+      }
     }
   }
-`
+`;
 
-const about = ({content}) => {
-    const { frontmatter, rawMarkdownBody } = content;
+const about = ({ content }) => {
+  const { frontmatter, rawMarkdownBody } = content;
 
   return (
     <StyledSection>
+      <AnimationOnScroll animateIn="animate__pulse">
         <StyledContentWrapper>
-            <div className='section-title'>{frontmatter.aboutTitle}</div>
+          <div className="inner-wrapper">
+            <h1 className="section-title">{frontmatter.aboutTitle}</h1>
             <div className="text-content">{rawMarkdownBody}</div>
+          </div>
         </StyledContentWrapper>
+      </AnimationOnScroll>
     </StyledSection>
-  )
-}
+  );
+};
 
-export default about
+export default about;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import * as React from "react"
-
 import Layout from "../components/Layout"
 import Hero from "../components/sections/Hero"
 import { graphql } from "gatsby"
