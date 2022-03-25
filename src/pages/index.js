38911ed08004489bc8ad2c890a5eeb65c2518843
mvpzl0b <zack.bellerose@gmail.com>
2022-03-25 09:38:25 -0400

    fixed errors for uploading to gatsby cloud

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import * as React from "react"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Hero from "../components/Hero"
 import { graphql } from "gatsby"
 
