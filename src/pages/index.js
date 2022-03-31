4a516f40997a41f56871db69fd738456bd0d21aa
Zackery <zack.bellerose@gmail.com>
2022-03-31 19:11:59 -0400

    temp remove of data

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import * as React from "react"
 import Layout from "../components/Layout"
 import Hero from "../components/sections/Hero"
-import { graphql } from "gatsby"
+//import { graphql } from "gatsby"
 import About from "../components/sections/about"
 import '../fonts/Orbitron-VariableFont_wght.ttf';
 
-const IndexPage = ({data}) => {
+const IndexPage = () => {
   return (  
     <Layout>
       {/* <Hero content={data.config.edges[0].node} />
@@ -16,20 +16,3 @@ const IndexPage = ({data}) => {
 
 export default IndexPage
 
-export const pageQuery = graphql`
-{
-  config: allMarkdownRemark {
-    edges {
-      node {
-        frontmatter {
-          heading
-          heroSubTitle
-          heroText
-          aboutTitle
-        }
-        rawMarkdownBody
-      }
-    }
-  }
-}
-`
